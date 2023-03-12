f40894308cd7931da9d47357910c8a64f1bacbae
Brogoming <kotag@outlook.com>
2023-03-12 16:52:14 -0500

    I finished the video
    It went more in depth with private posts and verification of user trying to login. I hope this helps me in my project I'm working on

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const postRoute = require('./routes/posts')
 
 dotenv.config()
 
@@ -16,6 +17,6 @@ app.use(express.json())
 
 //route middleware
 app.use('/api/user', authRoute) //everything in the authRoute will have this prefix
-
+app.use('/api/posts', postRoute)
 
 app.listen(3000, () => console.log('Server is up and running'))
